bda0909ece29de6a0f094e0f83bffc391468c361
bmosayebi <behnam.mosayebi80@gmail.com>
2023-01-20 18:04:13 +0330

    create main urls

---
+++ main/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = 'main'
+
+urlpatterns = [
+    path('surveys/', views.Surveys.as_view(), name="surveys"),
+    path('surveys/<int:survey_id>/', views.Survey.as_view(), name="survey"),
+
+    path('questions/', views.Questions.as_view(), name="questions"),
+    path('questions/<int:question_id>/', views.Question.as_view(), name="question"),
+
+    path('questionsChoices/', views.QuestionChoices.as_view(), name="question_choices"),
+    path('questionsChoices/<int:question_choice_id>/', views.QuestionChoice.as_view(), name="question_choice"),
+
+    path('questionImages/', views.QuestionImages.as_view(), name="question_images"),
+]
